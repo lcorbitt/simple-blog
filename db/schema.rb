f1b0be960c099d4bb1affbf0295bf54e4b751732
Lukas Corbitt <movalley5@gmail.com>
2021-05-22 13:13:24 -0600

    Changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_22_172137) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_172137) do
     t.text "description"
     t.boolean "published"
     t.datetime "published_at"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
